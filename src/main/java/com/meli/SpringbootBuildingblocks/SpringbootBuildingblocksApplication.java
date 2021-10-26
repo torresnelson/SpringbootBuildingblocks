1b2d876d5427427a92f333efdcf8b77a127b7cd8
Nelson Torres <nelson.torres@mercadolibre.com>
2021-10-26 09:47:09 -0300

    first REST controller

--- src/main/java/com/meli/SpringbootBuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/meli/SpringbootBuildingblocks/SpringbootBuildingblocksApplication.java
@@ -2,6 +2,7 @@ package com.meli.SpringbootBuildingblocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
---
+++ src/main/java/com/meli/SpringbootBuildingblocks/dto/UserDetails.java
@@ -0,0 +1,19 @@
+package com.meli.SpringbootBuildingblocks.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserDetails {
+    private String firstname;
+    private String lastname;
+    private String city;
+
+    @Override
+    public String toString() {
+        return "UserDetails [firstname = " + firstname + ", lastname = " + lastname + ", City = " + city +"]" ;
+    }
+}
